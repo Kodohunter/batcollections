6e2853aeabbeff9fc1d7abe2f62a38dccb405ebf
KodoHunter <lassi.hirvonen@pp.inet.fi>
2019-12-20 00:09:45 +0200

    Kärs - made to actually work, also better supported screen width

--- karsbat.bat
+++ karsbat.bat
@@ -1,3 +1,6 @@
-SET /A a = wmic desktopmonitor get screenwidth
+@echo off
 
-if(%a% gtr 2000) start steam://rungameid/731490
+for /f "tokens=2 delims==" %%f in ('wmic path Win32_VideoController get CurrentHorizontalResolution /value ^| find "="') do set /A "var=%%f"
+
+echo %var%
+if %var% GTR 2000 start steam://rungameid/731490
